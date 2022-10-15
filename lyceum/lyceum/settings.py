00e6c61eef3ff193d1c912d03edadc81923ae8ba
miheisv <misha.struko@mail.ru>
2022-10-15 21:56:50 +0300

    ready-to-rewiew
    ready-to-rewiew

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r8=%sluies-mn_y=k7x7_0mgd1@ksyapx#9unl\
-1f5!4v$w6oj'
+SECRET_KEY = 'django-insecure-r8=%sluies-mn_y=k7x7_0mgd1@ksyapx#9\
+unl1f5!4v$w6oj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

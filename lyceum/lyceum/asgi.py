4ae4dfe7b779e47f794489a54f7cb7f25387a3e0
miheisv <misha.struko@mail.ru>
2022-10-17 20:25:16 +0300

    com 5

--- lyceum/lyceum/asgi.py
+++ lyceum/lyceum/asgi.py
@@ -1,11 +1,4 @@
-"""
-ASGI config for lyceum project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
+"""ASGI config for lyceum project"""
 
 import os
 

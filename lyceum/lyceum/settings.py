6476d25c19eb0f731c3832a1133923956f10e4c6
miheisv <misha.struko@mail.ru>
2022-10-17 21:56:25 +0300

    com 7

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-r8=%sluies-mn_y=k7x7_0mgd1@ksyapx#9\
-unl1f5!4v$w6oj'
+SECRET_KEY = 'django-insecure-r8=%sluies-mn_y=k7x7_0mgd1@ksyapx#9'+
++'unl1f5!4v$w6oj'
 
 DEBUG = True
 
@@ -62,20 +62,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-            UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'+
+            +'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-            MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'+
+            +'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-            CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'+
+            +'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-            NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'+
+            +'NumericPasswordValidator',
     },
 ]
 

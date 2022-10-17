7bbb2e5a1d1b6626a94b14899b6f94d1305c96c8
miheisv <misha.struko@mail.ru>
2022-10-17 21:03:01 +0300

    com 6

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -4,16 +4,12 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
 SECRET_KEY = 'django-insecure-r8=%sluies-mn_y=k7x7_0mgd1@ksyapx#9\
 unl1f5!4v$w6oj'
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
 
 INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
@@ -57,16 +53,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lyceum.wsgi.application'
 
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -87,8 +79,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -99,9 +89,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-
 STATIC_URL = '/static/'
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

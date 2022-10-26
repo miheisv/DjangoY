98edfd5dcd6347aa3a910fdcbe45fd612ca00dc8
miheisv <misha.struko@mail.ru>
2022-10-26 19:04:59 +0300

    com 23

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.item_list),
-    path('<int:pk>/', views.item_detail),
+    re_path(r'(?P<pk>^[1-9]\d*)/$', views.item_detail),
 ]

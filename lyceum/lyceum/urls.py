d58147ca9e01f19ce57f4006d20c683dc5ce5068
miheisv <misha.struko@mail.ru>
2022-10-26 22:31:33 +0300

    сщь 28

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('homepage.urls', namespace=('homepage.urls', 'homepage'))),
-    path('catalog/', include('catalog.urls', namespace=('catalog.urls', 'catalog'))),
+    path('catalog/', include('catalog.urls', namespace=('catalog.urls',
+        'catalog'))),
     path('about/', include('about.urls', namespace=('about.urls', 'about'))),
     path('admin/', admin.site.urls),
 ]

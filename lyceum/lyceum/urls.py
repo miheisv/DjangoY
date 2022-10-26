b83af6e919befa0ce9c24ee0bb81779feee28bf3
miheisv <misha.struko@mail.ru>
2022-10-26 22:23:18 +0300

    com 26

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('homepage.urls', namespace='homepage', app_name='homepage')),
-    path('catalog/', include('catalog.urls', namespace='catalog', app_name='catalog')),
-    path('about/', include('about.urls', namespace='about', app_name='about')),
+    path('', include('homepage.urls', app_name='homepage')),
+    path('catalog/', include('catalog.urls', app_name='catalog')),
+    path('about/', include('about.urls', app_name='about')),
     path('admin/', admin.site.urls),
 ]

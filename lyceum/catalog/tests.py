bd77d1cb4b4618e7f56850e0c404271c43f41811
miheisv <misha.struko@mail.ru>
2022-10-26 18:42:08 +0300

    com 16

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -3,26 +3,26 @@ from django.test import TestCase, Client
 
 class StaticURLTests(TestCase):
     #endpoind url test
-    def tets_homepage_endpoint(self):
+    def test_homepage_endpoint(self):
         response = Client.get('/')
         self.assertEqual(response.status_code, 200)
     #endpoind url test
-    def tets_homepage_endpoin(self):
+    def test_homepage_endpoin(self):
         response = Client.get('/catalog/')
         self.assertEqual(response.status_code, 200)
     #endpoind url test
-    def tets_homepage_endpoi(self):
+    def test_homepage_endpoi(self):
         response = Client.get('/catalog/0/')
         self.assertEqual(response.status_code, 404)
     #endpoind url test
-    def tets_homepage_endpo(self):
+    def test_homepage_endpo(self):
         response = Client.get('/catalog/-1/')
         self.assertEqual(response.status_code, 404)
     #endpoind url test
-    def tets_homepage_endp(self):
+    def test_homepage_endp(self):
         response = Client.get('/catalog/abc1/')
         self.assertEqual(response.status_code, 404)
     #endpoind url test
-    def tets_homepage_end(self):
+    def test_homepage_end(self):
         response = Client.get('/catalog/1abc/')
         self.assertEqual(response.status_code, 404)
--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -3,6 +3,6 @@ from django.test import TestCase, Client
 
 class StaticURLTests(TestCase):
     #endpoind url test
-    def tets_homepage_endpoint(self):
+    def test_homepage_endpoint(self):
         response = Client.get('/')
         self.assertEqual(response.status_code, 200)

d8480f85ab62180436acbec4295448385816bb8e
miheisv <misha.struko@mail.ru>
2022-10-26 19:01:25 +0300

    com 22

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -5,30 +5,30 @@ class StaticURLTests(TestCase):
 
     # endpoind url test
     def test_homepage_endpoint(self):
-        response = Client.get('/')
+        response = Client().get(path='/')
         self.assertEqual(response.status_code, 200)
 
     # endpoind url test
     def test_homepage_endpoin(self):
-        response = Client.get('/catalog/')
+        response = Client().get(path='/catalog/')
         self.assertEqual(response.status_code, 200)
 
     # endpoind url test
     def test_homepage_endpoi(self):
-        response = Client.get(path='/catalog/0/')
+        response = Client().get(path='/catalog/0/')
         self.assertEqual(response.status_code, 404)
 
     # endpoind url test
     def test_homepage_endpo(self):
-        response = Client.get(path='/catalog/-1/')
+        response = Client().get(path='/catalog/-1/')
         self.assertEqual(response.status_code, 404)
 
     # endpoind url test
     def test_homepage_endp(self):
-        response = Client.get(path='/catalog/abc1/')
+        response = Client().get(path='/catalog/abc1/')
         self.assertEqual(response.status_code, 404)
 
     # endpoind url test
     def test_homepage_end(self):
-        response = Client.get(path='/catalog/1abc/')
+        response = Client().get(path='/catalog/1abc/')
         self.assertEqual(response.status_code, 404)

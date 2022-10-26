9ec95c7e25d664a96ce0665d0cbf85626954d37d
miheisv <misha.struko@mail.ru>
2022-10-26 22:28:44 +0300

    Update urls.py

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('homepage.urls', namespace='homepage.urls')),
-    path('catalog/', include('catalog.urls', namespace='catalog.urls')),
-    path('about/', include('about.urls', namespace='about.urls')),
+    path('', include('homepage.urls', namespace=('homepage.urls', 'homepage'))),
+    path('catalog/', include('catalog.urls', namespace=('catalog.urls', 'catalog'))),
+    path('about/', include('about.urls', namespace=('about.urls', 'about'))),
     path('admin/', admin.site.urls),
 ]

2fc516efe92ad5e601e18fda7284c38e1881871a
miheisv <misha.struko@mail.ru>
2022-10-31 18:43:46 +0300

    Update tests.py

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -38,7 +38,7 @@ class ModelsTests(TestCase):
     def test_able_create_one_letter(self):
         item_count = Item.objects.count()
 
-        with self.assertEqual(ValidationError):
+        with self.assertTrue(ValidationError):
             self.item = Item(
                 name='Тестовый товар',
                 category=self.category,

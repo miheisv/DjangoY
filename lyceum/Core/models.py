583f3f8220eb730ddb1a5cd00edc15cdba54c944
miheisv <misha.struko@mail.ru>
2022-10-31 11:23:31 +0300

    Update models.py

--- lyceum/Core/models.py
+++ lyceum/Core/models.py
@@ -12,10 +12,10 @@ class SlugMixInModel(models.Model):
     class Meta:
         abstract = True
     slug = models.CharField(
-                        max_length=200,
-                        unique=True,
-                        validators=[validate_slug]
-                        )
+        max_length=200,
+        unique=True,
+        validators=[validate_slug]
+    )
 
 
 class PublishedMixInModel(models.Model):

90e254bf74c5381f6fdcbf0597ca4a1b29f6c9a8
miheisv <misha.struko@mail.ru>
2022-10-31 18:22:53 +0300

    com 42

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -66,22 +66,26 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': ('django.contrib.auth.password_validation.'
+        'NAME': (
+            'django.contrib.auth.password_validation.'
             'UserAttributeSimilarityValidator'
         ),
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.'
+        'NAME': (
+            'django.contrib.auth.password_validation.'
             'MinimumLengthValidator'
         ),
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.'
+        'NAME': (
+            'django.contrib.auth.password_validation.'
             'CommonPasswordValidator'
         ),
     },
     {
-        'NAME': ('django.contrib.auth.password_validation.'
+        'NAME': (
+            'django.contrib.auth.password_validation.'
             'NumericPasswordValidator'
         ),
     },
